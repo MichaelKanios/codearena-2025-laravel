cc06f1c2e6129ebcbbdacbb955ade7044f710e4b
ΜΙΧΑΛΗΣ ΚΑΝΙΟΣ <michaleskanios@MICHALESs-Mini.home>
2025-05-13 22:19:02 +0300

    1st Test Pass

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,9 +7,13 @@
         <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Blog page</h2>
       </div>
       <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        @foreach($posts as $post)
-            <x-blog.post :post="$post" />
-        @endforeach
+        @if($posts->isEmpty())
+        <p>No posts found.</p>
+          @else
+            @foreach($posts as $post)
+              <x-blog.post :post="$post" />
+            @endforeach
+         @endif
       </div>
       <div class="mt-16">
         {{ $posts->links() }}
